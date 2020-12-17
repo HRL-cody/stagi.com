fa6b0b04f96923ae70cd104e011a59687fd5ec39
hamouda mdemagh <66480000+HRL-cody@users.noreply.github.com>
2020-12-17 01:36:42 +0100

    front commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Menu from "./components/Layout/LANDING"
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/Layout/Landing";
+import Navbar from "./components/Layout/Navbar";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route extact path="/" component={Landing} />
+      </Fragment>
+    </Router>
+  );
+}
 
-
-const App = () => 
-    <div className="App">
-      <Menu/>
-      
-    </div>
- 
 export default App;
--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -1,49 +1,21 @@
-import React from "react";
-import "./Landing.css";
+import React from 'react'
 
-const Menu = () => {
-  return (
-    <div>
-      <div className="header-dark" style={{ width: "85rem", height: "45rem" }}>
-        <nav className="navbar navbar-dark navbar-expand-lg navigation-clean-search">
-          <div className="container">
-            <a className="navbar-brand" href="www.google.com">
-              STAGI
-            </a>
-            <button
-              data-toggle="collapse"
-              data-target="#navcol-1"
-              className="navbar-toggler"
-            >
-              <span className="sr-only">Toggle navigation</span>
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse" id="navcol-1">
-              <form className="form-inline mr-auto" target="_self"></form>
-              <span className="navbar-text">
-                <a className="login" href="www.google.com">
-                  Log In
-                </a>
-              </span>
-              <a
-                className="btn btn-sucsess action-button"
-                role="button"
-                href="www.google.com"
-              >
-                Sign Up
-              </a>
-            </div>
-          </div>
-        </nav>
-        <div className="container hero">
-          <div className="row">
-            <div className="col-md-8 offset-md-2">
-              <h1 className="text-center">Start your Carer Here.</h1>
+const Landing = () => {
+    return (
+        <section className="landing">
+        <div className="dark-overlay">
+          <div className="landing-inner">
+            <h1 className="x-large">Stagi TN</h1>
+            <p className="lead">
+              Searrch for Your tranee here
+            </p>
+            <div className="buttons">
+              <a href="register.html" className="btn btn-primary">Sign Up</a>
+              <a href="login.html" className="btn btn-light">Login</a>
             </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+      </section>
+    );
 };
-export default Menu;
+export default Landing;
